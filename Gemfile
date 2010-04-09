ed75a31996e1241ab29ea260e78ddd7f43c8d0be
Ed Schmalzle <Jonas714@gmail.com>
2010-04-09 09:32:27 -0400

    install bundler and add basic requirements

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source :gemcutter
+
+gem "rails", "~> 2.3.5", :require => nil
+gem "haml"
+gem "compass"
+gem "compass-960-plugin"
+ 
+group :test do
+  gem 'factory_girl'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'webrat'
+  gem 'cucumber'
+  gem 'cucumber-rails'
+end
---
+++ config/preinitializer.rb
@@ -0,0 +1,13 @@
+begin
+  require "rubygems"
+  require "bundler"
+
+  if Gem::Version.new(Bundler::VERSION) <= Gem::Version.new("0.9.5")
+    raise RuntimeError, "Your bundler version is too old. Run `gem install bundler` to upgrade."
+  end
+
+  # Set up load paths for all bundled gems
+  Bundler.setup
+rescue Bundler::GemNotFound
+  raise Bundler::GemNotFound, "Bundler couldn't find some gems. Did you run `bundle install`?"
+end
