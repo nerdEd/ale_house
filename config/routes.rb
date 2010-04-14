092810cba9e9747d78fb0561395ede665674fc18
Flip Sasser <flip@x451.com>
2010-04-14 15:17:30 -0400

    Bmore on Ales stylesheet (I know, I know, it looks like a Mac)

--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "neighborhoods", :action => "index"
   
-  map.resources :neighborhoods do |n|
-    n.resources :ale_houses
+  map.resources :neighborhoods, :only => [:index] do |n|
+    n.resources :ale_houses, :only => [:index, :show]
   end
   
   map.resource :user_session
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,52 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  var convention_center = new google.maps.LatLng(39.285685,-76.616936);
+  var neighborhood_center = new google.maps.LatLng(39.283925,-76.597967);
+  
+  var map = new google.maps.Map(document.getElementById("map_canvas"), {
+    zoom: 14,
+    center: neighborhood_center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  
+  var marker = new google.maps.Marker({
+    position: convention_center, 
+    map: map,
+    icon: 'images/red_flag_30.png',
+    title: "Railsconf - at the Baltimore Convention Center"
+  });
+
+  var navLinks = $("#nav a"), listings = $('#ale_house_listing').css('overflow', 'hidden');
+
+  navLinks.click(function(event) {
+    event.preventDefault();
+    var link = $(this);
+    if (!link.hasClass('active')) {
+      // Hide listings box
+      listings.slideUp();
+      // Un-select active links
+      navLinks.removeClass('selected');
+      // Select active link and 
+      link.addClass('selected');
+      $.get(this.href, function(data) {
+        listings.html(data).slideDown(function() {
+          $('dl dt:first a').click();
+        });
+      });
+    }
+  });
+
+  navLinks.filter(':first').click();
+
+  $('dl dt a').live('click', function(event) {
+    event.preventDefault();
+    var link = $(this);
+    if (!link.hasClass('active')) {
+      $('dl a').removeClass('active');
+      link.addClass('active');
+      $('dl dd.active').removeClass('active').animate({height: 0, padding: 0}, function() {
+        $(this).css('display', '').css({height: '', padding: ''});
+      });
+      link.parents('dt').next().addClass('active').slideDown();
+    }
+  });
+});
