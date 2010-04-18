08f671b0a80b8ac53a0eeb342a1543149ce35b32
Sinclair Bain <sinclair.bain@gmail.com>
2010-04-18 00:47:43 -0400

    add view helper methods

--- app/models/ale_house.rb
+++ app/models/ale_house.rb
@@ -1,4 +1,9 @@
 class AleHouse < ActiveRecord::Base
   validates_presence_of :name, :address
   belongs_to :neighborhood
+  
+  def neighborhood_name()
+    neighborhood().name()
+  end
+  
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,4 +1,13 @@
 class Neighborhood < ActiveRecord::Base
   validates_presence_of :name
   has_many :ale_houses
+  
+  def self.new_ale_house(id)
+    find(id).new_ale_house()
+  end
+  
+  def new_ale_house()
+    ale_houses().new()
+  end
+  
 end
