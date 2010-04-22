e92367ab076e79ca8f5d99e1b5b112b1dd8043d5
Ed Schmalzle <Jonas714@gmail.com>
2010-04-22 10:33:32 -0400

    Generated authorization controller

---
+++ spec/controllers/authorizations_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe AuthorizationsController do
+
+  #Delete this example and add some real ones
+  it "should use AuthorizationsController" do
+    controller.should be_an_instance_of(AuthorizationsController)
+  end
+
+end
