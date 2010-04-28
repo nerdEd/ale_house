91cf41d1e9dc679836977f60470d81a8ee12ec67
Flip Sasser <flip@x451.com>
2010-04-27 20:53:51 -0400

    Holy fucking shit

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,7 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+config.gem 'rspec', :version => '1.2.9', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
+config.gem 'rspec-rails', :version => '1.2.9', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 
 require 'factory_girl'
