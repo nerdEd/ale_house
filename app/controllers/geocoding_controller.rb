da1d735bfbf7762f93a8df1ea15b830786d2a050
Sinclair Bain <sinclair.bain@gmail.com>
2010-04-28 21:41:13 -0400

    adding an address encoding

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -1,11 +1,14 @@
 require 'httpclient'
+require 'uri'
 
 class GeocodingController<ApplicationController
   
   def geocode()
+    address=URI.escape(params[:address] ) 
+    
     clnt = HTTPClient.new
     content = clnt.get_content(
-      "http://maps.google.com/maps/api/geocode/json?sensor=false&address=734+s+broadway,baltimore,md")
+      "http://maps.google.com/maps/api/geocode/json?sensor=false&address=#{address }")
     render :json=>content
 
   end
