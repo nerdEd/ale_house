dad60f238eb1456a2dc9a3e5b1733fda65eb6034
Sinclair Bain <sinclair.bain@gmail.com>
2010-04-28 21:57:42 -0400

    adding an address encoding and extraction

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -1,6 +1,7 @@
 require 'httpclient'
 require 'uri'
-
+require 'json'
+require 'PP'
 class GeocodingController<ApplicationController
   
   def geocode()
@@ -9,6 +10,7 @@ class GeocodingController<ApplicationController
     clnt = HTTPClient.new
     content = clnt.get_content(
       "http://maps.google.com/maps/api/geocode/json?sensor=false&address=#{address }")
+    content=JSON.parse(content)['results'][0]['geometry']['location'].to_json()
     render :json=>content
 
   end
